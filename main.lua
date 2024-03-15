c331f005826c03c978710857b5ac27e81f91c3ad
glassnerves <103482586+glassnerves@users.noreply.github.com>
2024-03-15 20:16:31 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -110,7 +110,7 @@ for post in fs.directory_iterator(INPUT / 'content' / 'post') do
             '--require', 'asciidoctor-diagram',
             '--attribute', 'ditaa-format=svg',
             '--attribute', 'plantuml-format=svg',
-            '--attribute', 'syntrax=jsyntrax-1.37/bin/syntrax',
+            '--attribute', 'java=jsyntrax-1.37/bin/syntrax',
             '--attribute', 'diagram-nocache-option',
 
             '--attribute', 'source-highlighter=rouge',
