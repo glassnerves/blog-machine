c47855f40dcf4a129f9a047af7a8b21968dbeafa
glassnerves <103482586+glassnerves@users.noreply.github.com>
2024-03-15 19:35:34 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -111,6 +111,7 @@ for post in fs.directory_iterator(INPUT / 'content' / 'post') do
             '--attribute', 'ditaa-format=svg',
             '--attribute', 'plantuml-format=svg',
             '--attribute', 'syntrax-format=svg',
+            '--attribute', 'java=../jsyntrax-1.37/bin/syntrax',
             '--attribute', 'diagram-nocache-option',
 
             '--attribute', 'source-highlighter=rouge',
